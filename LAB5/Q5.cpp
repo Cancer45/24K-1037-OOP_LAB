03e321f730d46dd773b0e9f0eabc644f68f1add3
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2025-03-03 10:38:16 +0500

    Update Q5.cpp

--- LAB5/Q5.cpp
+++ LAB5/Q5.cpp
@@ -1,6 +1,71 @@
 #include <iostream>
 //TO BE COMPLETED
-class Engine
+
+class Wheels //AGGREGATE THIS
+{
+    int TURN;
+
+    public:
+    Wheels () : TURN(0) {}
+
+    void turn ()
+    {
+        if (!TURN)
+        {
+            TURN = 1;
+            std::cout << "WHEELS TURNING" << std::endl;
+            return;
+        }
+
+        std::cout << "WHEELS ALREADY TURNING" << std::endl;
+    }
+
+    void stop ()
+    {
+        if (TURN)
+        {
+            TURN = 0;
+            std::cout << "WHEELS STOPPED" << std::endl;
+            return;
+        }
+
+        std::cout << "WHEELS ALREADY STOPPED" << std::endl;
+    }
+};
+
+class Headlight //AGGREGATE THIS
+{
+    int ON;
+
+    public:
+    Headlight () : ON(0) {}
+
+    void switchON ()
+    {
+        if (!ON)
+        {
+            ON = 1;
+            std::cout << "HEADLIGHTS SWITCHED ON" << std::endl;
+            return;
+        }
+
+        std::cout << "HEADLIGHTS ALREADY ON" << std::endl;
+    }
+
+    void switchOFF ()
+    {
+        if (ON)
+        {
+            ON = 0;
+            std::cout << "HEADLIGHTS SWITCHED OFF" << std::endl;
+            return;
+        }
+
+        std::cout << "HEADLIGHTS ALREADY OFF" << std::endl;
+    }
+};
+
+class Engine //COMPOSE THIS
 {
     int ON;
 
@@ -12,11 +77,11 @@ class Engine
         if (!ON)
         {
             ON = 1;
-            std::cout << "SWITCHED ON" << std::endl;
+            std::cout << "ENGINE SWITCHED ON" << std::endl;
             return;
         }
 
-        std::cout << "ALREADY ON" << std::endl;
+        std::cout << "ENGINE ALREADY ON" << std::endl;
     }
 
     void stop ()
@@ -24,19 +89,55 @@ class Engine
         if (ON)
         {
             ON = 0;
-            std::cout << "SWITCHED OFF" << std::endl;
+            std::cout << "ENGINE SWITCHED OFF" << std::endl;
             return;
         }
 
-        std::cout << "ALREADY OFF" << std::endl;
+        std::cout << "ENGINE ALREADY OFF" << std::endl;
     }
 };
 
-class Wheels;
-class Headlight;
-class Steering;
+class Steering //COMPOSE THIS
+{
+    int TURN;
+
+    public:
+    Steering () : TURN(0) {}
+
+    void turn ()
+    {
+        if (!TURN)
+        {
+            TURN = 1;
+            std::cout << "TURNING" << std::endl;
+            return;
+        }
+
+        std::cout << "ALREADY TURNING" << std::endl;
+    }
+
+    void reset ()
+    {
+        if (TURN)
+        {
+            TURN = 0;
+            std::cout << "RESET" << std::endl;
+            return;
+        }
+
+        std::cout << "ALREADY RESET" << std::endl;
+    }
+};
 
 class Car
 {
+    Engine engine();
+    Steering steering();
+    Headlight* headlight();
+    Wheels* wheels ();
 
+    public:
+    Car (Headlight* headlight, Wheels* wheels) : headlight(headlight), wheels(wheels) {}
+
+    //TO BE COMPLETED
 };
