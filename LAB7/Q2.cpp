1394a09bf71d5dee07a68adc83fde2471b8ec1a0
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2025-03-17 10:20:03 +0500

    Update Q2.cpp

--- LAB7/Q2.cpp
+++ LAB7/Q2.cpp
@@ -8,7 +8,52 @@ class Position
 
 class Shape
 {
+    protected:
     Position position;
     std::string color;
-    float borderThickness;    
-}
+    float borderThickness;
+
+    public:
+    Shape () {}
+    Shape (float X, float Y, std::string color, float borderThickness) : color(color), borderThickness(borderThickness) 
+    {
+        position.X = X;
+        position.Y = Y;
+    }    
+
+    void draw ()
+    {
+        std::cout << "drawing shape" << std::endl;
+    }
+
+    void calculateArea ()
+    {
+        std::cout << "calculating shape area" << std::endl;
+    }
+
+    void calculatePerimeter ()
+    {
+        std::cout << "calculating perimeter" << std::endl;
+    }
+};
+
+class Circle : public Shape
+{
+    float Radius;
+
+};
+
+class Rectangle : public Shape
+{
+
+};
+
+class Triangle : public Shape
+{
+
+};
+
+class Polygon : public Shape
+{
+    
+};
